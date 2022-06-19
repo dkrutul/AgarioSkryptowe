e1df6dbc7c189c6f01723b360525db6cf48ab8f0
Dawid Krutul <dawtul01@gmail.com>
2022-06-19 16:25:47 +0200

    Szpachlowanie nr 1

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ top_left_x = (s_width - play_width) // 2
 top_left_y = s_height - play_height
 
 # SHAPE FORMATS
+# wszystkie kombinacje danych figur, niektore sa symetryczne wiec bedzie ich mniej
 
 S = [['.....',
       '......',
@@ -121,15 +122,23 @@ shapes = [S, Z, I, O, J, L, T]
 shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
 
 
-# index 0 - 6 represent shape
-
-
 class Piece(object):
-    pass
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+        self.color = shape_colors[shapes.index(shape)]
+        self.rotation = 0
 
 
-def create_grid(locked_positions={}):
-    pass
+def create_grid(locked_pos={}):
+    grid = [[(0, 0, 0) for x in range(10)] for x in range(20)]  # kolorowanie planszy 10 x 20
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (j, i) in locked_pos:
+                grid[i][j] = locked_pos[(j, i)]  # kolorujemy na dany kolor jesli juz cos stoi w danym polu
+    return grid
 
 
 def convert_shape_format(shape):
@@ -144,16 +153,22 @@ def check_lost(positions):
     pass
 
 
-def get_shape():
-    pass
+def get_shape(shapes):
+    return Piece(5, 0, random.choice(shapes))  # losujemy figure
 
 
 def draw_text_middle(text, size, color, surface):
     pass
 
 
-def draw_grid(surface, row, col):
-    pass
+def draw_grid(surface, grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            pygame.draw.rect(surface, grid[i][j],
+                             (top_left_x + j * block_size, top_left_y + i * block_size, block_size, block_size),
+                             0)  # block_size = 30 default
+
+    pygame.draw.rect(surface, (0, 255, 0), (top_left_x, top_left_y, play_width, play_height), 4)
 
 
 def clear_rows(grid, locked):
@@ -164,16 +179,59 @@ def draw_next_shape(shape, surface):
     pass
 
 
-def draw_window(surface):
-    pass
+def draw_window(surface, grid):
+    surface.fill((0, 0, 0))
+
+    pygame.font.init()
+    font = pygame.font.SysFont('Arial', 60)
+    label = font.render('Tetris', True, (144, 132, 13))
+
+    surface.blit(label, (top_left_x + play_width / 2 - (label.get_width() / 2), 30))  # zabawa zeby to bylo na srodku
+
+    draw_grid(surface, grid)
+    pygame.display.update()
 
 
-def main():
-    pass
+def main(win):
+    locked_positions = {}
+    grid = create_grid(locked_positions)
+
+    change_piece = False
+    run = True
+    current_piece = get_shape(shapes)
+    next_piece = get_shape(shapes)
+    clock = pygame.time.Clock()
+    fall_time = 0
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    current_piece.x -= 1
+                    if not(valid_space(current_piece, grid)):
+                        current_piece += 1
+                if event.key == pygame.K_RIGHT:
+                    current_piece.x += 1
+                    if not(valid_space(current_piece, grid)):
+                        current_piece -= 1
+                if event.key == pygame.K_DOWN:
+                    current_piece.y += 1
+                    if not(valid_space(current_piece, grid)):
+                        current_piece.y -= 1
+                if event.key == pygame.K_UP:
+                    current_piece.rotation += 1
+                    if not(valid_space(current_piece, grid)):
+                        current_piece -= 1
+
+        draw_window(win, grid)
+
+def main_menu(win):
+    main(win)
 
 
-def main_menu():
-    pass
-
-
-main_menu()  # start game
+win = pygame.display.set_mode((s_width, s_height))
+pygame.display.set_caption('Tetris')
+main_menu(win)  # start game
