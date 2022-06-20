405fd5c1ba5494f9b8293c4af76f1c557d782256
Dawid Krutul <dawtul01@gmail.com>
2022-06-20 09:46:28 +0200

    wykończeniówka

--- main.py
+++ main.py
@@ -159,7 +159,7 @@ def convert_shape_format(shape):
 
 def valid_space(shape, grid):
     accepted_pos = [[(j, i) for j in range(10) if grid[i][j] == (0, 0, 0)] for i in range(20)]
-    accepted_pos = [j for sub in accepted_pos for j in sub] #przejscie z 2d do 1d
+    accepted_pos = [j for sub in accepted_pos for j in sub]  # przejscie z 2d do 1d
 
     formatted = convert_shape_format(shape)
 
@@ -169,12 +169,12 @@ def valid_space(shape, grid):
                 return False
     return True
 
+
 def check_lost(positions):
     for pos in positions:
         x, y = pos
         if y < 1:
             return True
-
     return False
 
 
@@ -182,8 +182,11 @@ def get_shape(shapes):
     return Piece(5, 0, random.choice(shapes))  # losujemy figure
 
 
-def draw_text_middle(text, size, color, surface):
-    pass
+def draw_text_middle(surface, text, size, color):
+    font = pygame.font.SysFont("Arial", size, bold=True)
+    label = font.render(text, 1, color)
+
+    surface.blit(label, (top_left_x + play_width / 2  - (label.get_width() / 2), top_left_y + play_height / 2 - label.get_height() / 2))
 
 
 def draw_grid(surface, grid):
@@ -197,9 +200,8 @@ def draw_grid(surface, grid):
 
 
 def clear_rows(grid, locked):
-
-    inc = 0 #ilosc rzedow
-    for i in range(len(grid) - 1, -1, -1): #czyscimy rzędy od dołu
+    inc = 0  # ilosc rzedow
+    for i in range(len(grid) - 1, -1, -1):  # czyscimy rzędy od dołu
         row = grid[i]
         if (0, 0, 0) not in row:
             inc += 1
@@ -211,12 +213,31 @@ def clear_rows(grid, locked):
                     continue
 
     if inc > 0:
-        for key in sorted(list(locked), key= lambda x: x[1])[::-1]: #sortujemy po y i odwracamy
+        for key in sorted(list(locked), key=lambda x: x[1])[::-1]:  # sortujemy po y i odwracamy
             x, y = key
             if y < ind:
-                newKey = (x ,y + inc)
+                newKey = (x, y + inc)
                 locked[newKey] = locked.pop(key)
 
+    return inc
+
+
+def update_score(newScore):
+    score = max_score()
+
+    with open('scores.txt', 'w') as file:
+        if newScore > int(score):
+            file.write(str(newScore))
+        else:
+            file.write(str(score))
+
+
+def max_score():
+    with open('scores.txt', 'r') as file:
+        lines = file.readlines()
+        score = lines[0].strip()
+
+    return score
 
 def draw_next_shape(shape, surface):
     font = pygame.font.SysFont('Arial', 30)
@@ -231,20 +252,38 @@ def draw_next_shape(shape, surface):
         row = list(line)
         for j, column in enumerate(row):
             if column == '0':
-                pygame.draw.rect(surface, shape.color, (sx + j * block_size, sy + i * block_size, block_size, block_size), 0)
+                pygame.draw.rect(surface, shape.color,
+                                 (sx + j * block_size, sy + i * block_size, block_size, block_size), 0)
 
     surface.blit(label, (sx - 20, sy - 50))
 
 
-
-def draw_window(surface, grid):
+def draw_window(surface, grid, score=0, last_score=0):
     surface.fill((0, 0, 0))
 
     pygame.font.init()
+
     font = pygame.font.SysFont('Arial', 60)
-    label = font.render('Tetris', True, (144, 132, 13))
+    label = font.render('Tetris vibes', True, (144, 132, 13))
 
     surface.blit(label, (top_left_x + play_width / 2 - (label.get_width() / 2), 30))  # zabawa zeby to bylo na srodku
+
+    #curr score
+    font = pygame.font.SysFont('Arial', 30)
+    label = font.render('Score: ' + str(score), True, (30, 60, 90))
+
+    sx = top_left_x + play_width + 50
+    sy = top_left_y + play_height / 2
+
+    surface.blit(label, (sx + 10, sy + 140))
+
+    #last score
+    label = font.render('High Score: ' + str(last_score), True, (30, 60, 90))
+
+    sx = top_left_x - 250
+    sy = top_left_y + 200
+
+    surface.blit(label, (sx + 10, sy + 140))
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
@@ -255,12 +294,13 @@ def draw_window(surface, grid):
     pygame.draw.rect(surface, (0, 255, 0), (top_left_x, top_left_y, play_width, play_height), 5)
 
     draw_grid(surface, grid)
-    #pygame.display.update()
+    # pygame.display.update()
 
 
 def main(win):
     locked_positions = {}
     grid = create_grid(locked_positions)
+    last_score = max_score()
 
     change_piece = False
     run = True
@@ -268,20 +308,27 @@ def main(win):
     next_piece = get_shape(shapes)
     clock = pygame.time.Clock()
     fall_time = 0
-    fall_speed = 0.27
+    fall_speed = 0.3
+    level_time = 0
+    score = 0
 
     while run:
         grid = create_grid(locked_positions)
         fall_time += clock.get_rawtime()
+        level_time += clock.get_rawtime()
         clock.tick()
+
+        if level_time / 1000 > 5: #co 5s gra przyspiesza
+           if fall_speed > 0.006:
+               fall_speed -= 0.005
+           level_time = 0
 
         if fall_time / 1000 > fall_speed:
             fall_time = 0
             current_piece.y += 1
-            if not(valid_space(current_piece, grid)) and current_piece.y > 0:
+            if not (valid_space(current_piece, grid)) and current_piece.y > 0:
                 current_piece.y -= 1
                 change_piece = True
-
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -290,21 +337,20 @@ def main(win):
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     current_piece.x -= 1
-                    if not(valid_space(current_piece, grid)):
+                    if not (valid_space(current_piece, grid)):
                         current_piece.x += 1
                 if event.key == pygame.K_RIGHT:
                     current_piece.x += 1
-                    if not(valid_space(current_piece, grid)):
+                    if not (valid_space(current_piece, grid)):
                         current_piece.x -= 1
                 if event.key == pygame.K_DOWN:
                     current_piece.y += 1
-                    if not(valid_space(current_piece, grid)):
+                    if not (valid_space(current_piece, grid)):
                         current_piece.y -= 1
                 if event.key == pygame.K_UP:
                     current_piece.rotation += 1
-                    if not(valid_space(current_piece, grid)):
+                    if not (valid_space(current_piece, grid)):
                         current_piece.rotation -= 1
-
 
         shape_pos = convert_shape_format(current_piece)
 
@@ -316,29 +362,42 @@ def main(win):
         if change_piece:
             for pos in shape_pos:
                 p = (pos[0], pos[1])
-                locked_positions[p] = current_piece.color #np [ (1,2):(155,233,233) ]
+                locked_positions[p] = current_piece.color  # np [ (1,2):(155,233,233) ]
 
             current_piece = next_piece
             next_piece = get_shape(shapes)
             change_piece = False
-            clear_rows(grid, locked_positions)
+            score += clear_rows(grid, locked_positions) * 10
 
-        draw_window(win, grid)
+        draw_window(win, grid, score, last_score)
         draw_next_shape(next_piece, win)
         pygame.display.update()
 
         if check_lost(locked_positions):
+            draw_text_middle(win, " You lost :( ", 80, (200, 100, 50))
+            pygame.display.update()
+            pygame.time.delay(1500)
             run = False
+            update_score(score)
+
+
+def main_menu(win):
+    run = True
+    while run:
+        win.fill((0, 0, 0))
+        draw_text_middle(win, 'Press any key to play', 60, (50, 100, 150))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.display.quit()
+            if event.type == pygame.KEYDOWN:
+                main(win)
 
     pygame.display.quit()
 
-def main_menu(win):
-    main(win)
-
-
 win = pygame.display.set_mode((s_width, s_height))
 pygame.display.set_caption('Tetris')
-main_menu(win)  # start game
-
+main_menu(win)
 
 
